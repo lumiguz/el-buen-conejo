88866091280f7e58d68bbcf38da523a2d7455ba3
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-10-09 19:46:05 -0400

    feat: form start

--- frontend/src/Routes/Landing/index.jsx
+++ frontend/src/Routes/Landing/index.jsx
@@ -1,14 +1,12 @@
 import Heading from "../../UI/Heading";
 import Button from "../../UI/Button";
 import Navbar from "../../Containers/Navbar";
-import LittersCards from "../../Components/LittersCards/LittersCards";
 const index = () => {
   return (
     <div className="container bg-light">
       <Navbar />
       <Heading>Landing Page</Heading>
       <Button>Botón verde success</Button>
-      <LittersCards littersName="Alex y Mary" littersAge="2" littersImage="" littersWeight="10" littersKit="2"/>
     </div>
   );
 };
