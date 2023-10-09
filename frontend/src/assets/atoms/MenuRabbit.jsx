311faea87252bb6309e72f218db840db85b92a70
Keiber Hernández <115053607+Keialex17@users.noreply.github.com>
2023-10-09 02:46:04 +0000

    lista pagination

--- frontend/src/assets/atoms/MenuRabbit.jsx
+++ frontend/src/assets/atoms/MenuRabbit.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import MenuRabbitPerfil from "./MenuRabbitPerfil";
 import "../atoms/menuRabbit.css"
 
+function PaginatedView({ currentPage, onPageChange}){
+  const pages = [
+    { id: "perfil", label: "Perfil" },
+    { id: "camadas", label: "Camadas" },
+    { id: "notas", label: "Notas" },
+  ];
+
+  return (
+    <div>
+      {/* Paginación personalizada */}
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page.id}
+            className={`page-item ${page.id === currentPage ? "active" : ""}`}
+            onClick={() => onPageChange(page.id)}
+          >
+            <a className="page-link" href="#">
+              {page.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 const MenuRabbit = () =>{
+
+  const [currentPage, setCurrentPage] = useState("perfil");
+
+  const handlePageChange = (pageId) => {
+    // Actualiza la página actual cuando se hace clic en la paginación personalizada
+    setCurrentPage(pageId);
+  };
+
+  let content;
+  if (currentPage === "perfil") {
+    content = <div>
+      <MenuRabbitPerfil/>
+    </div>;
+  } else if (currentPage === "camadas") {
+    content = <div>Contenido de la página 2</div>;
+  } else if (currentPage === "notas") {
+    content = <div>Contenido de la página 3</div>;
+  }
+
     return <>
     <div>
-    <nav aria-label="Page navigation example">
-  <ul className="pagination" id="pagination">
-    <li className="page-item"><a className="page-link" href="#">Perfil</a></li>
-    <li className="page-item"><a className="page-link" href="#">Camadas</a></li>
-    <li className="page-item"><a className="page-link" href="#">Notas</a></li>
-  </ul>
-</nav>
+      <div>
+        <PaginatedView currentPage={currentPage} onPageChange={handlePageChange} />
+       </div>
+       {content}
     </div>
     </>
 }
---
+++ frontend/src/assets/atoms/MenuRabbitPerfil.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MenuRabbitPerfil = () =>{
+    return <>
+    <div>
+        <h6> Datos del conejo: </h6>
+        <table className="table">
+  <thead>
+    <tr>
+      <th scope="col" className="table-secondary">Estado</th>
+      <th scope="col">Vivo</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Peso</th>
+      <td>8 kg</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    </>
+}
+
+export default MenuRabbitPerfil
