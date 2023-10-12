203f65eef11df341a5113a12f506d10956d75000
Yarumis <yarumisginanrm@gmail.com>
2023-10-12 18:07:32 -0400

    cardCategory insertada

--- frontend/src/Routes/Market/index.jsx
+++ frontend/src/Routes/Market/index.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import Heading from "../../UI/Heading";
-import Button from "../../UI/Button";
+import ButtonRecientes from "../../UI/ButtonRecientes";
+import FilterMarket from "../../UI/FilterMarket";
 import Navbar from "../../Containers/Navbar";
 import CardMarket from "../../Components/CardMarket"
+import CardCategory from "../../Components/CardCategory/CardCategory"
+import Footer from '../../Components/Footer';
+
 const index = () => {
-    return (
-      <div className="container">
-        <Navbar />
-        <Heading>Market</Heading>
-        <CardMarket/>
+  const AllCategories = ["MiniLop", "Blanc de Hotot", "Conejo Rex", "Gigantes de flandes"]
+
+  const [categories, setCategories] = useState(AllCategories);
+
+  return (
+    <div className="container m-5">
+      <Navbar />
+      <Heading>Buscar por razas</Heading>
+      <div className="row grid-1">
+        <CardCategory categories={categories} />
       </div>
-    );
-  };
-  
-  export default index;
+      <div className='d-flex justify-content-between my-2'>
+        <ButtonRecientes />
+        <FilterMarket />
+      </div>
+      <div className="row grid-1">
+        <CardMarket />
+        <CardMarket />
+        <CardMarket />
+        <CardMarket />
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default index;
