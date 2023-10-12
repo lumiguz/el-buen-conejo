e032e891c77620b168993303d9e7552d793e20a9
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-10-12 13:12:49 -0500

    add route litters
    it is add teh route /litters in the Navbar component

--- frontend/src/Containers/Navbar/index.jsx
+++ frontend/src/Containers/Navbar/index.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
     { name: "Comunidad", href: "/community" },
     { name: "Mercado", href: "/market" },
     { name: "Eventos y Recursos", href: "/events" },
+    { name: "Camadas", href: "/litters" },
     { name: "Iniciar sesión", href: "/login", className: "text-primary" },
   ];
 
