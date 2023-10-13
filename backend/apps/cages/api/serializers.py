7e5f3b2e249f52ff36192145bcb86b40370662b0
Yeiner Parra <yeinerparrabernal@gmail.com>
2023-10-13 15:52:59 -0500

    created basic serializer

--- backend/apps/cages/api/serializers.py
+++ backend/apps/cages/api/serializers.py
@@ -2,42 +2,49 @@ from rest_framework import serializers
 from apps.cages.models import Cage
 
 class CageSerializer(serializers.ModelSerializer):
-    price = serializers.DecimalField(max_digits=5, decimal_places=2)
+    #price = serializers.DecimalField(max_digits=5, decimal_places=2)
     class Meta:
         model = Cage
         fields = "__all__"
 
-    def validate_count_rabbits(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
-        return value
+#     def validate_count_rabbits(self, value):
+#         if value < 0:
+#             raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
+#         return value
 
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
-        return value
+#     def validate_price(self, value):
+#         if value < 0:
+#             raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
+#         return value
 
-class CageListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cage
-        fields = ("id", "farm_id", "count_rabbits", "price", "is_public", "is_active", "photo")
+# class CageListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cage
+#         fields = ("id", "farm_id", "count_rabbits", "price", "is_public", "is_active", "photo",)
+#     def to_representation(self, instance):
+#         return {
+#             "id": instance["id"],
+#             "count_rabbits": instance["count_rabbits"],
+#             "price": instance["price"],
+#             "photo": instance["photo"].url 
+#         }
+        
+# class CageUpdateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cage
+#         fields = ("count_rabbits", "price", "is_public", "photo",)
 
-class CageUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cage
-        fields = ("count_rabbits", "price", "is_public", "photo")
+# class CageCreateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cage
+#         fields = ("farm_id", "count_rabbits", "price", "photo",)
 
-class CageCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cage
-        fields = ("farm_id", "count_rabbits", "price", "photo")
+#     def validate_count_rabbits(self, value):
+#         if value < 0:
+#             raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
+#         return value
 
-    def validate_count_rabbits(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
-        return value
-
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
-        return value
+# # #     def validate_price(self, value):
+#         if value < 0:
+#             raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
+#         return value
