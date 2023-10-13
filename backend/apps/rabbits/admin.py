bf02f0aa63aeb225b550e26764282ab4158ae5ba
LuMi <lumigu.dev@gmail.com>
2023-10-12 23:25:02 -0500

    Rabbit admin created

--- backend/apps/rabbits/admin.py
+++ backend/apps/rabbits/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.rabbits.models import Rabbit
 
-# Register your models here.
+
+class RabbitAdmin(admin.ModelAdmin):
+    # list_display = ("", "", "", "")
+    # search_fields = ("", "")
+    # ordering = ("",)
+    pass
+
+
+admin.site.register(Rabbit, RabbitAdmin)
