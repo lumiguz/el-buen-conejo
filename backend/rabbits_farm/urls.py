edb8f4e67e18028b36ee6a8af0f0b8ab7f7ce9d3
LuMi <lumigu.dev@gmail.com>
2023-10-12 22:43:50 -0500

    Added rabbit api path

--- backend/rabbits_farm/urls.py
+++ backend/rabbits_farm/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("login/", Login.as_view(), name="login"),
     path("logout/", Logout.as_view(), name="logout"),
     re_path(r"^api/", include("apps.users.api.routers")),
+    re_path(r"^api-rabbit/", include("apps.rabbits.api.routers")),
 ]
