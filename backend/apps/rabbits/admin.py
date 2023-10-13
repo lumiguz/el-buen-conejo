882f8eec84c51f824518c6fb02e3b1cda1895354
LuMi <lumigu.dev@gmail.com>
2023-10-13 18:49:41 -0500

    Admin display and search created

--- backend/apps/rabbits/admin.py
+++ backend/apps/rabbits/admin.py
@@ -3,9 +3,25 @@ from apps.rabbits.models import Rabbit
 
 
 class RabbitAdmin(admin.ModelAdmin):
-    # list_display = ("", "", "", "")
-    # search_fields = ("", "")
-    # ordering = ("",)
+    list_display = (
+        "id",
+        "cage_id",
+        "breed",
+        "genre",
+        "price",
+        "tag",
+        "weight",
+    )
+    search_fields = (
+        "id",
+        "cage_id",
+        "breed",
+        "genre",
+        "price",
+        "tag",
+        "weight",
+    )
+    ordering = ("breed",)
     pass
 
 
