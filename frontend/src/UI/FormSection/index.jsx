dad62d36049390da0a8772fafbac98ee81ccaee8
Alejou343 <alejandro.auribe1@gmail.com>
2023-10-13 19:15:26 -0500

    Formulacion de inputs asociados a Login

--- frontend/src/UI/FormSection/index.jsx
+++ frontend/src/UI/FormSection/index.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 const index = ({type, id, placeholder, label, onChange}) => {
   return (
     <div className="mb-2">
-        <label htmlFor={id} className="form-label">{label}</label>
-        <input 
-          type={type} 
-          className="form-control" 
-          id={id} 
-          placeholder={placeholder}
-          onChange={onChange}
-        />
+      <label htmlFor={id} className="form-label">{label}</label>
+      <input 
+        type={type} 
+        className="form-control"
+        aria-describedby="inputGroupPrepend"
+        id={id} 
+        placeholder={placeholder}
+        onChange={onChange}
+        required
+      />
+      <div class="invalid-feedback">
+        Please choose a username.
+      </div>
     </div>
   )
 }
