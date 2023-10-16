29cef8471905f1a7c081daa23df6778624ecff7d
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-10-16 19:56:50 -0400

    feat: Added content to litters page, added css modules to some components

--- frontend/src/Routes/Litters/index.jsx
+++ frontend/src/Routes/Litters/index.jsx
@@ -1,31 +1,95 @@
-// import LittersCard from '../../Components/LittersCards/LittersCard';
-import LittersForm from '../../Components/LittersForm/LittersForm';
-import LittersRabbitsCard from '../../Components/LittersRabbitsCard/LittersRabbitsCard';
-import LittersProfileButton from '../../UI/LittersProfileButton/LittersProfileButton';
+import Footer from '../../Components/Footer';
+import LittersCard from '../../Components/LittersCard';
+import LittersForm from '../../Components/LittersForm';
+// import LittersRabbitsCard from '../../Components/LittersRabbitsCard';
+import Profile from '../../Components/Profile';
+import Navbar from '../../Containers/Navbar';
 
 const Litters = () => {
   return (
-    <div className='m-4'>
-      {/* Litters
-      <LittersCard
-        littersName='{nombre example}'
-        littersAge='2'
-        littersImage=''
-        littersWeight='10'
-        littersKit='2'
-  /> */}
-      <LittersProfileButton>Ver Perfil</LittersProfileButton>
+    <div>
+      <Navbar />
+      <div className='m-3 '>
+        <div className={`d-flex flex-wrap mb-3 bg-light`}>
+          <Profile />
+        </div>
 
-      <LittersForm />
+        <div className='d-inline-flex flex-wrap justify-content-center row row-cols-lg-3 row-cols-sm-1 row-cols-md-2'>
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={1}
+          />
 
-      <LittersRabbitsCard
-        rabbitName='Mario'
-        rabbitPhoto=''
-        rabbitBreed='California'
-        rabbitWeight='10'
-        rabbitAge='2'
-        sold={true}
-      />
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={2}
+          />
+
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={3}
+          />
+
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={4}
+          />
+
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={5}
+          />
+
+          <LittersCard
+            littersName='{nombre example}'
+            littersAge='2'
+            littersImage=''
+            littersWeight='10'
+            littersKit='2'
+            litterBreed='California'
+            litterId={6}
+          />
+        </div>
+        {/* <LittersRabbitsCard
+          rabbitName='Mario'
+          rabbitPhoto=''
+          rabbitBreed='California'
+          rabbitWeight='10'
+          rabbitAge='2'
+          sold={false}
+          rabbitId={1}
+        /> */}
+
+        <div className='d-flex flex-column  flex-wrap justify-content-center align-items-center'>
+          <LittersForm />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
