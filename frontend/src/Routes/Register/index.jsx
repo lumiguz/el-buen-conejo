36cf7a86f4601ab158d707e393b39c24e762a04c
Alejou343 <alejandro.auribe1@gmail.com>
2023-10-16 19:27:45 -0500

    Feature: Ahora las rutas asociadas a Login contienen el Navbar y Footer

--- frontend/src/Routes/Register/index.jsx
+++ frontend/src/Routes/Register/index.jsx
@@ -6,22 +6,28 @@ import RegisterForm from '../../Components/RegisterForm'
 import Heading from '../../UI/Heading'
 import Paragraph from '../../UI/Paragraph'
 import AppLink from '../../UI/AppLink'
+import Navbar from '../../Containers/Navbar'
+import Footer from '../../Components/Footer'
 
 const index = () => {
     return (
-        <div className="w-50 justify-content-center mx-auto border p-3">
-            <Heading className="text-center"> Comienza a ser productor </Heading>
-            <Paragraph className="text-center"> Primero crea tu cuenta en El buen conejo </Paragraph>
-            <Paragraph className="text-center"> 
-                ¿Ya tienes una cuenta? &nbsp;
-                <AppLink href="/login">Inicia sesión</AppLink>
-            </Paragraph>
-            <RegisterForm />
-            <Paragraph className="text-center my-3 text-success text-sm">
-                Al continuar, usted reconoce que ha leído, entendido y está de acuerdo con las &nbsp;
-                <AppLink href="/" className="text-decoration-none">Condiciones del servicio</AppLink>
-                &nbsp; y la Política de privacidad de El buen conejo.
-            </Paragraph>
+        <div className="container">
+            <Navbar />
+                <div className="w-50 justify-content-center mx-auto border p-3 my-4">
+                    <Heading className="text-center"> Comienza a ser productor </Heading>
+                    <Paragraph className="text-center"> Primero crea tu cuenta en El buen conejo </Paragraph>
+                    <Paragraph className="text-center"> 
+                        ¿Ya tienes una cuenta? &nbsp;
+                        <AppLink href="/login">Inicia sesión</AppLink>
+                    </Paragraph>
+                    <RegisterForm />
+                    <Paragraph className="text-center my-3 text-success text-sm">
+                        Al continuar, usted reconoce que ha leído, entendido y está de acuerdo con las &nbsp;
+                        <AppLink href="/" className="text-decoration-none">Condiciones del servicio</AppLink>
+                        &nbsp; y la Política de privacidad de El buen conejo.
+                    </Paragraph>
+                </div>
+            <Footer />
         </div>
     )
 }
