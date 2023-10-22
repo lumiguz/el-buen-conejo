6a54857c746f16bc9e0dc36552f5a79da13ca595
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-10-22 18:36:57 -0500

    refactoring
    refactoring MenuRabbit

--- frontend/src/Components/MenuRabbit/MenuRabbit.jsx
+++ frontend/src/Components/MenuRabbit/MenuRabbit.jsx
@@ -1,26 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import MenuRabbitPerfil from "./MenuRabbitPerfil";
-import styles from "../MenuRabbit/menuRabbit.module.css"
-import MenuRabbitCamada from "./MenuRabbitCamada"
+import styles from "../MenuRabbit/menuRabbit.module.css";
+import MenuRabbitCamada from "./MenuRabbitCamada";
 import MenuRabbitNotes from "./MenuRabbitNotes";
 
-function PaginatedView({ currentPage, onPageChange}){
+function PaginatedView({ currentPage, onPageChange }) {
   const pages = [
     { id: "perfil", label: "Perfil" },
     // { id: "camadas", label: "Camadas" },
     { id: "notes", label: "Notas" },
   ];
-
-  const rabbitData = {
-    estado: "Vivo",
-    peso: "8 kg",
-    raza: "California",
-    edad: "8 meses",
-    color: "Blanco",
-    genotipo: "-",
-    criasVivas: "4",
-    totalDeCrias: "14",
-  };
 
   return (
     <div>
@@ -45,13 +34,15 @@ function PaginatedView({ currentPage, onPageChange}){
   );
 }
 
-const MenuRabbit = () =>{
+const MenuRabbit = () => {
   const [perfilActive, setPerfilActive] = useState(true);
   const [notasActive, setNotasActive] = useState(false);
-  
+
   useEffect(() => {
     // Cargar el estado desde el LocalStorage
-    const storedPaginationState = JSON.parse(localStorage.getItem('paginationState'));
+    const storedPaginationState = JSON.parse(
+      localStorage.getItem("paginationState")
+    );
     if (storedPaginationState) {
       setPerfilActive(storedPaginationState.perfilActive);
       setNotasActive(storedPaginationState.notasActive);
@@ -60,7 +51,10 @@ const MenuRabbit = () =>{
 
   useEffect(() => {
     // Guardar el estado en el LocalStorage
-    localStorage.setItem('paginationState', JSON.stringify({ perfilActive, notasActive }));
+    localStorage.setItem(
+      "paginationState",
+      JSON.stringify({ perfilActive, notasActive })
+    );
   }, [perfilActive, notasActive]);
 
   const handlePageChange = (pageId) => {
@@ -73,20 +67,31 @@ const MenuRabbit = () =>{
     }
   };
 
-    return <>
-    <div>
-      <div>
-        <PaginatedView 
-        
-        currentPage={perfilActive ? "perfil" : "notes"}
-          onPageChange={handlePageChange} 
-          
-          />
-       </div>
-       {perfilActive && <MenuRabbitPerfil rabbitData={rabbitData} />}
-      {notasActive && <MenuRabbitNotes />}
-    </div>
-    </>
-}
+  const rabbitData = {
+    estado: "Vivo",
+    peso: "8 kg",
+    raza: "California",
+    edad: "8 meses",
+    color: "Blanco",
+    genotipo: "-",
+    criasVivas: "4",
+    totalDeCrias: "14",
+  };
 
-export default MenuRabbit
+  return (
+    <>
+      <div>
+        <div>
+          <PaginatedView
+            currentPage={perfilActive ? "perfil" : "notes"}
+            onPageChange={handlePageChange}
+          />
+        </div>
+        {perfilActive && <MenuRabbitPerfil rabbitData={rabbitData} />}
+        {notasActive && <MenuRabbitNotes />}
+      </div>
+    </>
+  );
+};
+
+export default MenuRabbit;
