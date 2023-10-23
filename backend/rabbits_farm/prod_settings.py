8e5974d4e07f225c5f2f7d65d2458ddb602f23ef
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2023-10-23 15:38:16 -0600

    Deploy preparation
    Preparated deploy in production with AWS EC2 and nginx

--- backend/rabbits_farm/prod_settings.py
+++ backend/rabbits_farm/prod_settings.py
@@ -10,23 +10,6 @@ if "RDS_HOSTNAME" in os.environ:
 
 DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("RDS_DB_NAME"),
-        "USER": os.environ.get("RDS_USERNAME"),
-        "PASSWORD": os.environ.get("RDS_PASSWORD"),
-        "HOST": os.environ.get("RDS_HOSTNAME"),
-        "PORT": os.environ.get("RDS_PORT"),
-    }
-}
-
 # Configuration of django-cors-headers
 # CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
--- backend/rabbits_farm/settings.py
+++ backend/rabbits_farm/settings.py
@@ -14,32 +14,3 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, True)
-)
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-print("E U R E K A -- BASE_DIR: " + os.path.join(BASE_DIR, ".env"))
-
-# False if not in os.environ because of casting above
-DEBUG = env("DEBUG")
-print(" RED PILL:", DEBUG)
-# Raises Django's ImproperlyConfigured
-# exception if SECRET_KEY not in os.environ
-SECRET_KEY = env("SECRET_KEY")
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("POSTGRES_NAME"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST"),
-        "PORT": env("POSTGRES_PORT"),
-    }
-}
