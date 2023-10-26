3f0652ca496ce53e568d1e5e40405d5ef2bf486a
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-10-25 19:29:22 -0500

    add useHttpGetWithPagination and endpoints Api El buen conejo
    to use custom hook for the pagination in the endpoints and recfactor some components

--- frontend/src/Routes/Market/index.jsx
+++ frontend/src/Routes/Market/index.jsx
@@ -1,71 +1,26 @@
 import Heading from "../../UI/Heading";
-import { allRabbitBreeds } from "../../utils/market/RabbitData";
 import CardImage from "../../Components/CardImage";
 import AppLink from "../../UI/AppLink";
-import { useHttp } from "../../hooks/useHttp";
-import { useEffect } from "react";
+import { BREED_CHOICE } from "../../utils/breeds";
 
 const Market = () => {
-  const { isLoading, error, data, sendRequest } = useHttp();
-  useEffect(() => {
-    sendRequest(`http://107.21.219.35/api/rabbits/`);
-  }, [sendRequest]);
-
   return (
     <>
-      {isLoading && (
-        <h2 className="text-muted text-center m-5 p-5">Cargando...</h2>
-      )}
-      {error && (
-        <h2 className="text-danger text-center m-5 p-5">
-          Ha ocurrido un error, intente de nuevo
-        </h2>
-      )}
-      {!isLoading && data && (
-        <>
-          {data.results.length === 0 && (
-            <h2 className="text-muted text-center m-5 p-5">
-              No hay conejos disponibles
-            </h2>
-          )}
-          {data.results.length !== 0 && (
-            <section>
-              <div className="d-flex justify-content-between my-2">
-                <select className="btn btn-outline-dark p-1">
-                  <option value="1">Recientes</option>
-                  <option value="2">Conejo 1</option>
-                  <option value="3">Conejo 2</option>
-                </select>
-                <select className="btn btn-outline-dark  p-1">
-                  <option value="1">filtrar</option>
-                  <option value="2">Conejo 1</option>
-                  <option value="3">Conejo 2</option>
-                </select>
-              </div>
-
-              <div className="row">
-                {data.results.map((rabbit) => (
-                  <div
-                    key={rabbit.id}
-                    className="my-2 col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center"
-                  >
-                    <CardImage
-                      image={rabbit.photo}
-                      title={rabbit.tag}
-                      text={rabbit.price}
-                      link={{
-                        url: `/market/${rabbit.id}`,
-                        text: "Ubicación",
-                        className: "text-muted",
-                      }}
-                    />
-                  </div>
-                ))}
-              </div>
-            </section>
-          )}
-        </>
-      )}
+      <section>
+        <Heading className="text-center m-4">Buscar por razas</Heading>
+        <div className="row">
+          {BREED_CHOICE.map((breed, index) => (
+            <div
+              key={index}
+              className="my-2 col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center"
+            >
+              <AppLink href={`/market/${breed.name}`}>
+                <CardImage image={breed.urlImage} title={breed.name} />
+              </AppLink>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
