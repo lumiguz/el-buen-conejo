a4acd40c2cae8c8569990aa0395f4d7cb680735d
Yeiner Parra <yeinerparrabernal@gmail.com>
2023-10-26 21:41:10 -0500

    deleted validations

--- backend/apps/cages/api/serializers.py
+++ backend/apps/cages/api/serializers.py
@@ -5,25 +5,26 @@ class CageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cage
         fields = "__all__"
+        read_only_fields = ( "id", "price", "count_rabbits", "total_weight", "created",)
 
     # count_rabbits validation
-    def validate_count_rabbits(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
-        return value
+    # def validate_count_rabbits(self, value):
+    #     if value < 0:
+    #         raise serializers.ValidationError("El número de conejos debe ser igual o mayor que 0.")
+    #     return value
 
-    # price validation
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
-        return value
+    # # price validation
+    # def validate_price(self, value):
+    #     if value < 0:
+    #         raise serializers.ValidationError("El precio debe ser igual o mayor que 0.")
+    #     return value
     
-    # price validation if count_rabbits > 0
-    def validate(self, data):
-        count_rabbits = data.get('count_rabbits')
-        price = data.get('price')
+    # # price validation if count_rabbits > 0
+    # def validate(self, data):
+    #     count_rabbits = data.get('count_rabbits')
+    #     price = data.get('price')
         
-        if count_rabbits > 0 and price == 0:
-            raise serializers.ValidationError("El precio debe ser mayor que 0 si hay conejos en la jaula.")
+    #     if count_rabbits > 0 and price == 0:
+    #         raise serializers.ValidationError("El precio debe ser mayor que 0 si hay conejos en la jaula.")
         
-        return data
+    #     return data
