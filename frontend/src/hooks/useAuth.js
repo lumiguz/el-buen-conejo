b2d72721ee0d58b81ead42d45c8d730fdf415668
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-10-28 23:08:54 -0500

    refactoring code
    the code is optimized in the section de market and the navbar

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,14 +1,6 @@
 import { useLocation } from "react-router-dom";
-import { useMyContext } from "../context/AppContextProvider";
 
 export function useIsAuthPath(authPaths) {
     const location = useLocation();
     return authPaths.some(path => location.pathname.includes(path));
-}
-
-export function useIsLoggedIn() {
-    // your code for isLoggedIn is here
-    const usuario = useMyContext();
-    console.log(usuario);
-    return usuario !== null;
 }
