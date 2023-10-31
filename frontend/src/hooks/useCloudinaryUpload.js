3a81c44b75f469375514a0857de36f545e0629bd
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-10-30 20:01:32 -0500

    i created custom hook named useCloudinaryUpload.js
    this hook is for upload the images in cloudinary and get the url

---
+++ frontend/src/hooks/useCloudinaryUpload.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const useCloudinaryUpload = (uploadPreset, cloudName) => {
+    const [imageUrl, setImageUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const uploadToCloudinary = async (image) => {
+
+        setLoading(true);
+        const formData = new FormData();
+        formData.append('file', image);
+        formData.append('upload_preset', uploadPreset);
+
+        try {
+            const response = await fetch(
+                `https://api.cloudinary.com/v1_1/${cloudName}/upload`,
+                {
+                    method: 'POST',
+                    body: formData,
+                }
+            );
+
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                setImageUrl(jsonResponse.secure_url);
+            } else {
+                console.log('Server responded with status:', response.status);
+            }
+        } catch (err) {
+            console.error('Error uploading image to Cloudinary:', err);
+        }
+
+        setLoading(false);
+    };
+
+    return { uploadToCloudinary, imageUrl, loading };
+};
+
+export default useCloudinaryUpload;
