4a33c310e8fc8a502f5c29ebdc0f4f54e26cd7c1
antoniPrz <antoni.protoni@gmail.com>
2023-11-01 10:58:35 -0300

    fix
    profile

--- frontend/src/Routes/ProfileHome/index.jsx
+++ frontend/src/Routes/ProfileHome/index.jsx
@@ -2,8 +2,15 @@
 import MenuProfile from "../../Components/MenuProfile/MenuProfile";
 import Profile from "../../Components/Profile";
 
+const cuenta = JSON.parse(localStorage.getItem('logedAccount'))
+console.log(cuenta)
+console.log(typeof(cuenta))
+const userId = localStorage.getItem("userId")
+console.log("El id del usuario es:", userId)
+
 
 const ProfileHome =  () =>{
+
 
     return(
         <>
